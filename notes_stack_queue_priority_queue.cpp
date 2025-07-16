aab29c228947c607dd7a37ee720d27e696ed9aad
Gurparas5 <161712798+Gurparas5@users.noreply.github.com>
2025-07-16 13:14:13 +0530

    Add files via upload

--- notes_stack_queue_priority_queue.cpp
+++ notes_stack_queue_priority_queue.cpp
@@ -14,3 +14,92 @@ Stack:
 		3. top(): Returns the element at the top of the stack without removing it.
 		4. empty(): Returnns true if stack is empty otherwise false.
 		5. size(): return number of elements of stack.
+		
+
+
+Polish Notation:
+	This type of notation was introduced by the Polish mathematician Lukasiewicz. 
+	- Polish notation in data structure tells us about different ways to write an 
+		arithmetic expression. 
+		
+	- An arithmetic expression contains 2 things:
+		1. Operator:
+			Operator is a symbol that represents a specific operation to be performed.
+			eg:
+				+, -, *, /, ^, %
+
+		
+		2. Operand:	
+			Operand is a value or a variable on which an opertion is performed by an operator.
+			eg:
+				0 to 9, a to z, A to Z
+	
+	
+	
+	- There are mainly three type of Polish Notation:
+		1. Infix notation:
+			This polish notation in data structure states that the operator is written 
+			in between the operands.
+			
+			eg:
+				( a + b ) x c ( a + b ) x c
+				a + b - c + d
+				( a x b ) + ( c / d ) ( a x b )
+			
+			
+		2. Prefix notation:
+			This notation in data structure states that the operator should be present as a
+			prefix or before the operands.
+			eg:
+				  + a b
+				  + - a b c + - a b c
+				  x a b x a b
+			
+			
+		3. Postfix notation:
+			This notation in data structure states that the operator should be present as a
+			after the operands.
+			
+			- postfix notation is a computer understandable format.
+			
+			eg:
+				 a b -
+				 a b / c d /
+				 12 5 + 3 / 12 5 + 3 /
+				 8 2 3 x + 8 2 3 x +
+	
+	
+	
+	
+Queue:
+	Queue is a linear data structure that stores the data in First In First Out order.
+	eg:
+		printing pages in printer.
+		downloading files in some application.
+		
+	- Operations implemented on the queue:
+		1. enqueue(): adding an element to the end of the queue.
+		2. dequeue(): Removing an element from the front of the queue.
+		3. front():	Retrieving the element at the front of the queue without removing it.
+		4. rear(): Retrieving the element at the rear of the queue without removing it.
+		5. empty(): return true if queue is empty, otherwise it return false.
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+		
