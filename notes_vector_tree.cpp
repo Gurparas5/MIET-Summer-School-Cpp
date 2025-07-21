f408d862eafae236ad6eb184e48ee8317e01285d
Gurparas5 <161712798+Gurparas5@users.noreply.github.com>
2025-07-21 18:44:53 +0530

    Add files via upload

---
+++ notes_vector_tree.cpp
@@ -0,0 +1,85 @@
+Vector:
+	In C++ Dynamic array is implemented using the std::vector container from
+	Standard Template Library(STL).
+	
+	- We can change the sizeof vector during runtime.
+	
+	Important functions:
+		1. push_back():	// push new value at the end of vector.
+		2. pop_back();	// pop() out the last element.
+		3. size();	// return the number of elements.
+		4. empty();	// It return true if vector have no element
+		5. clear();	// pop() out all the elemnts of vector
+		6. at();	// return the value at index.
+		7. front();	// return the value of 0th index element.
+		8. back();	// return the value of 1st index element.
+		9. begin(); // return the reference of 0th index element
+		10. end(); // return the reference of just after last element
+
+
+Tree:
+	Tree data structure is a hierarchical structure that consists of nodes connected
+	by edges. It is a widely used abstract data type that simulates a hierarchical tree
+	structure, with root value and subtrees of childrens.
+	
+	Node: Each element in a tree is called a node. it contains a vlaur or data.
+	
+	Root: The topmost node in a tree is called the root. 
+			- It is the only node that has no parent.
+	
+	Edge: The connection between two nodes is called an edge.
+		
+	Child: A node that is directly connected to another node which moving away from the 
+				root is called child.
+				
+	Parent: A node that has one or more child nodes is called a parent.
+		
+	Leaf:
+		A node that does not have any child is called a leaf or terminal node.
+		
+	Subtree:
+	Depth:
+	Height:
+	Level:
+
+
+	
+# Tree Traversing:
+	There are mainly two type of traversing in a tree:
+		1. DFS:	Depth-first search it explores as far as possible along each branch 
+					before backtracking.
+		
+		2. BFS: Breadth first search explores neighbors before moving to the next level,
+					typically using queue.
+	
+	In DFS they are of three type:
+		1.	Pre-Order Traversal
+		2.	In-Order Traversal
+		3.	Post-Order Traversal		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
